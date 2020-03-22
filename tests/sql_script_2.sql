78f5cb6dc040958ed188c5100ec6da31d5ca5bd3
Guzman Monne <guzmonne@hotmail.com>
2020-03-22 19:26:27 -0300

    fixed bugs

--- tests/sql_script_2.sql
+++ tests/sql_script_2.sql
@@ -1,3 +1,3 @@
-CREATE USER {{ sql_test_1_username }} WITH ENCRYPTED PASSWORD '{{ sql_test_1_password }}';
-CREATE DATABASE {{ sql_test_1_database_name }};
-GRANT ALL PRIVILEGES ON DATABASE {{ sql_test_1_database_name }} TO {{ sql_test_1_username }};
+CREATE USER {{ sql_test_2_username }} WITH ENCRYPTED PASSWORD '{{ sql_test_2_password }}';
+CREATE DATABASE {{ sql_test_2_database_name }};
+GRANT ALL PRIVILEGES ON DATABASE {{ sql_test_2_database_name }} TO {{ sql_test_2_username }};
